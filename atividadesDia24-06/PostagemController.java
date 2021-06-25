e3b639338710410d85d09c6e46512f6376774e86
gabriela paganini <gabrielapaganini67@gmail.com>
2021-06-24 21:23:22 -0300

    tinha tudo pra dar certo mas AINDA não deu

---
+++ atividadesDia24-06/PostagemController.java
@@ -0,0 +1,17 @@
+package atividade.gen.blogPessoal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import atividade.gen.blogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+}
---
+++ atividadesDia24-06/PostagemRepository.java
@@ -0,0 +1,12 @@
+package atividade.gen.blogPessoal.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import atividade.gen.blogPessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+		public List<Postagem> findAllByContainingIgnoreCase (String titulo);
+	}
+
