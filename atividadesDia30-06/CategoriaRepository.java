3bdc8bad9efea841fed6f700ffdb4921020bda94
gabriela paganini <gabrielapaganini67@gmail.com>
2021-06-30 22:42:17 -0300

    incluir @JsonIgnoreProperties.

---
+++ atividadesDia30-06/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.minhaloja.games.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.minhaloja.games.model.Categoria;
+
+	@Repository
+	public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+			public List<Categoria> findAllByJogoContainingIgnoreCase (String jogo);
+
+	
+		}
+
