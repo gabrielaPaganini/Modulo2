0fbcfae5d3cb72029de60761e27a43370aa7e8c7
gabriela paganini <gabrielapaganini67@gmail.com>
2021-07-10 17:35:59 -0300

    Basic Security Loja de games

---
+++ spring security/model/Categoria.java
@@ -0,0 +1,52 @@
+package com.minhaloja.games.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String descricao;
+	
+	@OneToMany(mappedBy = "produto", cascade = CascadeType.ALL)
+	private List<Produto> produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
---
+++ spring security/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.minhaloja.games.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.minhaloja.games.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public Optional<Usuario> findByUsuario(String usuario);
+}
